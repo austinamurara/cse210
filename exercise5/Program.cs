154d902e05b539c46b1c0c643380165aed97432c
austinamurara <muraraaustina83@gmail.com>
2026-03-11 09:26:38 +0200

    Exercise 5: Finalized functions project

---
+++ exercise5/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+// This is the "Main" part that calls the functions
+DisplayWelcome();
+
+string userName = PromptUserName();
+int userNumber = PromptUserNumber();
+
+int squaredNumber = SquareNumber(userNumber);
+
+DisplayResult(userName, squaredNumber);
+
+// --- These are the Functions ---
+
+static void DisplayWelcome()
+{
+    Console.WriteLine("Welcome to the program!");
+}
+
+static string PromptUserName()
+{
+    Console.Write("Please enter your name: ");
+    return Console.ReadLine();
+}
+
+static int PromptUserNumber()
+{
+    Console.Write("Please enter your favorite number: ");
+    return int.Parse(Console.ReadLine());
+}
+
+static int SquareNumber(int number)
+{
+    return number * number;
+}
+
+static void DisplayResult(string name, int square)
+{
+    Console.WriteLine($"{name}, the square of your number is {square}");
+}
