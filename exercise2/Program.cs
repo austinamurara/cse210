8512ae7138b1445c212768282d93ca7ad4f1c70f
austinamurara <muraraaustina83@gmail.com>
2026-03-11 08:35:59 +0200

    Exercise 2 is running and perfect

---
+++ exercise2/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Ask the user for their grade percentage
+        Console.Write("Enter your grade percentage: ");
+        string input = Console.ReadLine();
+        int grade = int.Parse(input);
+
+        // Determine letter grade
+        string letter;
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        // Determine if passed
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("Keep trying! You can do better next time.");
+        }
+
+        // Determine + or - sign
+        string sign = "";
+        int lastDigit = grade % 10;
+
+        if (letter != "A" && letter != "F") // exclude A+ and F+/- 
+        {
+            if (lastDigit >= 7)
+            {
+                sign = "+";
+            }
+            else if (lastDigit < 3)
+            {
+                sign = "-";
+            }
+        }
+        else if (letter == "A" && lastDigit < 3) // A-
+        {
+            sign = "-";
+        }
+
+        // Print final grade
+        Console.WriteLine("Your final grade is: " + letter + sign);
+    }
+}
